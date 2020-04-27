8d0745f5a446496a36c406a10e0b20e111d76e94
ProudlyTM <m.ivanov2k@gmail.com>
2020-04-27 14:00:24 +0300

    Initial version

---
+++ womic.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: womic [OPTIONS]"
+    echo "Options:"
+    echo "   -b  -  to connect via Bluetooth"
+    echo "   -w  -  to connect via WiFi"
+    echo "   -h  -  help (display this message)"
+    exit 1
+fi
+
+if [ `ls | grep ^micclient-x86_64_.*$` ]; then
+    MICCLIENT=$(ls | grep ^micclient-x86_64_.*$)
+else
+    echo "WO Mic Appimage binary not found! Make sure you have this script in the same directory as the binary"
+    exit 1
+fi
+
+function executable_check() {
+    if ! [ -x $MICCLIENT ]; then
+        chmod +x $MICCLIENT
+    fi
+}
+
+function module_check() {
+    if ! [ `lsmod | grep -o ^snd_aloop` ]; then
+        sudo modprobe snd-aloop
+    fi
+}
+
+while getopts "hbwk" OPTION; do
+    case $OPTION in
+        h)
+            echo "Usage: womic [OPTIONS]"
+            echo "Options:"
+            echo "   -b     to connect via Bluetooth"
+            echo "   -w     to connect via WiFi"
+            echo "   -h     help (this message)"
+            exit 0
+            ;;
+        b)
+            executable_check
+            module_check
+            # WIP
+            echo "Do stuff"
+            exit 0
+            ;;
+        w)
+            executable_check
+            module_check
+            printf "Enter device IP: "
+            read IP
+            echo ""
+            # TODO - Need to fix this part below, it's not working properly yet
+            ./$MICCLIENT -t Wifi $IP &> /tmp/womic.status &
+            sleep 1
+            echo $! > /tmp/womic.pid
+            tail -n0 -f /tmp/womic.status | sed '/^Connected$/q; /^Error: Fail to connect to server.$/q'
+            if [ `grep "Connected" /tmp/womic.status` ]; then
+                printf "\nUse './womic.sh -k' to disconnect\n"
+                exit 0
+            else
+                exit 1
+            fi
+            ;;
+        k)
+            if ! [ `ps -p $(cat /tmp/womic.pid) > /dev/null 2>&1` ]; then
+                printf "WO Mic is not running!\n"
+                exit 1
+            else
+                kill -2 $(cat /tmp/womic.pid)
+                sleep 1
+                exit 0
+            fi
+            ;;
+        *)
+            echo ""
+            echo "Run 'womic -h' for help"
+            exit 0
+            ;;
+    esac
+done
